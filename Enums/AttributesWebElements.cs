cdeccb214ebc9e780bd814685b724de82029d59d
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-06-08 14:54:37 +0300

    Add files via upload

---
+++ Enums/AttributesWebElements.cs
@@ -0,0 +1,46 @@
+﻿
+namespace MyLibraries.MyEthernetLib.Enums
+{
+    /// <summary>
+    /// Атрибути веб елементів
+    /// </summary>
+    public enum AttributesWebElements
+    {
+        /// <summary>
+        /// Унікальний ідентифікатор 
+        /// </summary>
+        Id,
+        /// <summary>
+        /// Клас
+        /// </summary>
+        Class,
+        /// <summary>
+        /// Посилання
+        /// </summary>
+        Href,
+        /// <summary>
+        /// Ім'я файла-джерела
+        /// </summary>
+        Src,
+        /// <summary>
+        /// Довжина
+        /// </summary>
+        Width,
+        /// <summary>
+        /// Висота
+        /// </summary>
+        Height,
+        /// <summary>
+        /// Заголовок
+        /// </summary>
+        Title,
+        /// <summary>
+        /// Альтернативний текст
+        /// </summary>
+        Alt,
+        /// <summary>
+        /// Рядковий контейнер
+        /// </summary>
+        Span
+    }
+}
